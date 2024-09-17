27e6c5dc4b6f1e966d18b6379b7f98ae26def432
Timi Ombe <timiombe@Timis-MacBook-Air.local>
2024-09-17 13:36:53 +0100

    added video in hero section

--- 1 - getstructure/src/components/Hero.jsx
+++ 1 - getstructure/src/components/Hero.jsx
@@ -12,8 +12,18 @@ function Hero() {
                 We take care of your financial operations so you can lead your startup more effectively.
             </p>
         </div>
+
+        <div className='flex items-center pt-14 pb-10'>
+          <span className='h-[1px] bg-black w-full'></span>
+        </div>
+
+        <div className='flex justify-center bg-red-100 '>
+          <video className='rounded-3xl' autoPlay={true} muted loop >
+            <source className='' src='https://cdn.prod.website-files.com/6448cf5dd9cba41a73db0e5f/644e8628a0c93f42d1928ce7_Masthead-transcode.mp4'/>
+          </video>
+        </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
